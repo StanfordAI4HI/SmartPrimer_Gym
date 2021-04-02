0a472a9e9b19428dac7be5ccce17f0b811fd6c33
William Steenbergen <39959357+WilliamSteenbergen@users.noreply.github.com>
2021-04-02 11:16:24 -0700

    Add files via upload

---
+++ gym_SmartPrimer/__init__.py
@@ -0,0 +1,38 @@
+from gym.envs.registration import register
+
+register(
+    id='SmartPrimer-deterministic-v1',
+    entry_point='gym_SmartPrimer.envs.V1:SmartPrimerEnv',
+)
+register(
+    id='SmartPrimer-medium-v1',
+    entry_point='gym_SmartPrimer.envs.V1:SmartPrimerMediumEnv',
+)
+register(
+    id='SmartPrimer-realistic-v1',
+    entry_point='gym_SmartPrimer.envs.V1:SmartPrimerHardEnv',
+)
+register(
+    id='SmartPrimer-dynamic-v1',
+    entry_point='gym_SmartPrimer.envs.V1:SmartPrimerDynamicEnv',
+)
+
+register(
+    id='SmartPrimer-realistic-v2',
+    entry_point='gym_SmartPrimer.envs.V2:SmartPrimerDynamicEnv',
+)
+
+register(
+    id='SmartPrimer-realistic-v3',
+    entry_point='gym_SmartPrimer.envs.V2:SmartPrimerSherryEnv',
+)
+
+register(
+    id='SmartPrimer-knownPersonas-v2',
+    entry_point='gym_SmartPrimer.envs.V2:SmartPrimerKnownEnv',
+)
+
+register(
+    id='TestEnv-v0',
+    entry_point='gym_SmartPrimer.envs.V2:TestEnv',
+)
